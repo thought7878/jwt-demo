32a2d69f8dba9c11d9a00c58789fb89f7fb585af
Calm Andy <thought7878@qq.com>
2019-01-19 11:21:36 +0800

    add signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const { PORT, DB_PATH } = require('./config')
 const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+// middleware
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
 
 // DB
 const mongoose = require('mongoose')
@@ -12,6 +18,18 @@ db.on('connected', () => console.log('mongo connected'))
 // api
 app.get('/', (req, res) => {
   res.send('hello client')
+})
+const User = require('./models/user')
+app.post('/user/signup', async (req, res) => {
+  try {
+    const user = new User(req.body)
+    await user.save()
+    res.json({
+      msg: 'user saved'
+    })
+  } catch (error) {
+    res.status(500).json({ msg: 'signup error', error })
+  }
 })
 
 app.listen(PORT, () => console.log(`server is running at port ${PORT}`))
