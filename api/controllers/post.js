093c9cfeb91ca721ba66b2374ea0e7ddd78aa7bb
Calm Andy <thought7878@qq.com>
2019-02-10 11:19:58 +0800

    add 文章API

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,23 @@
-exports.new = (req, res) => {
-  res.json({ msg: '认证通过了！' })
+const Post = require('../models/post')
+exports.new = async (req, res) => {
+  try {
+    const post = new Post(req.body)
+    await post.save()
+    res.json({
+      id: post._id,
+      title: post.title,
+      body: post.body
+    })
+  } catch (error) {
+    res.status(500).json({ msg: '保存错误' })
+  }
+}
+
+exports.posts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    res.status(500).json({ msg: '服务器出错了' })
+  }
 }
--- api/routes.js
+++ api/routes.js
@@ -3,7 +3,10 @@ const Post = require('./controllers/post')
 const { requireAuth } = require('./utils/jwt')
 
 module.exports = app => {
+  // user
   app.post('/user/signup', User.signup)
   app.post('/user/login', User.login)
+  // post
   app.post('/post', requireAuth, Post.new)
+  app.get('/posts', Post.posts)
 }
